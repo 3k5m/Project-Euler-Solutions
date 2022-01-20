a51baee3adb7774a84b51e9a7bb9cb30a56b0bf2
3k5m <96869224+3k5m@users.noreply.github.com>
2022-01-20 22:14:05 +0900

    Create Problem3.cpp
    Added problem 3

---
+++ Source/Problem3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isprime(int num){
+  if(num == 2||num == 3) return true;
+  for(int i=2;i * i < num;i++){
+    if(num%i==0){
+      return false;
+    }
+  }
+  return true;
+}
+
+int main() {
+  long long number = 600851475143 , factor = 2;
+  while(number!=1){
+    while(number%factor == 0){
+      number = number / factor;
+    }
+    do{
+      factor++;
+    }while(!isprime(factor));
+  }
+  cout<<factor;
+} 
